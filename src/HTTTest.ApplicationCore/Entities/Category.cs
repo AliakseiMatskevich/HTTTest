c11efbcef29a6c003b5b15b8eff16a078ab5da45
Aliaksei Matskevich <mats2@tut.by>
2023-06-22 10:53:49 +0300

    Add Entities BaseEntity, Category, Product

---
+++ src/HTTTest.ApplicationCore/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using HTTTest.ApplicationCore.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTTest.ApplicationCore.Entities
+{
+    public sealed class Category : BaseEntity
+    {
+        [MaxLength(5)]
+        public string? Measure { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ src/HTTTest.ApplicationCore/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using HTTTest.ApplicationCore.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTTest.ApplicationCore.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string? Description { get; set; }
+        public int Weight { get; set; }
+        public decimal Price { get; set; }
+        public Category? Category { get; set; }
+    }
+}
