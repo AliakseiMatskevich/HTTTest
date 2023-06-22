cd287a94c82b3ff4822e4ab760850b944ea9ab36
Aliaksei Matskevich <mats2@tut.by>
2023-06-22 14:51:37 +0300

    Add Generic repository

---
+++ src/HTTTest.ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTTest.ApplicationCore.Interfaces
+{
+    public interface IRepository<T>  where T : class
+    {
+        Task<IList<T>> GetListAsync(Expression<Func<T, bool>>? predicate = null,
+                Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                Func<IQueryable<T>, IIncludableQueryable<T, object>>? includes = null);
+    }
+}
