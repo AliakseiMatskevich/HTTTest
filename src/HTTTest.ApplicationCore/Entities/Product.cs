819786c63f0eb9d2e6878f41a44371beb2c5d57e
Aliaksei Matskevich <mats2@tut.by>
2023-06-22 14:27:15 +0300

    Add DbContext, Migration, Seed Database

--- src/HTTTest.ApplicationCore/Entities/Product.cs
+++ src/HTTTest.ApplicationCore/Entities/Product.cs
@@ -1,6 +1,7 @@
 ﻿using HTTTest.ApplicationCore.Entities.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,15 @@ namespace HTTTest.ApplicationCore.Entities
 {
     public class Product : BaseEntity
     {
+        [MaxLength(500)]
         public string? Description { get; set; }
+        [Range(0, int.MaxValue)]
         public int Weight { get; set; }
+        [Range(0, (double)decimal.MaxValue)]
         public decimal Price { get; set; }
+        public Guid CategoryId { get; set; }
         public Category? Category { get; set; }
+        public string? PictureUrl { get; set; }
+
     }
 }
--- src/HTTTest.Web/Program.cs
+++ src/HTTTest.Web/Program.cs
@@ -1,9 +1,44 @@
+using HTTTest.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+#region Configuration DbContext
+builder.Services.AddDbContext<HTTTestContext>(context =>
+            context.UseSqlServer(builder.Configuration.GetConnectionString("HTTTestConnection")));
+#endregion
+
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages()
+    .AddRazorPagesOptions(options => {
+        options.Conventions.AddPageRoute("/Product/Index", "");
+    }); ;
 
 var app = builder.Build();
+
+app.Logger.LogInformation("App created...");
+
+#region Migrations running and Seed
+using (var scope = app.Services.CreateScope())
+{
+    var scopedProvider = scope.ServiceProvider;
+    try
+    {
+        var context = scopedProvider.GetRequiredService<HTTTestContext>();
+        if (context.Database.IsSqlServer())
+        {
+            app.Logger.LogInformation("Database migration running...");
+            context.Database.Migrate();
+        }
+        await HTTTestContextSeed.SeedAsync(context, app.Logger);
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "An error occurred adding migrations to Database.");
+    }
+}
+#endregion
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
